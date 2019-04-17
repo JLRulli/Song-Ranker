15a8e602474a51ee13f645c893bb346138187f83
jacob <jacob@128.211.188.46>
2019-04-17 15:42:10 -0400

    converted to maven for firebase

--- SongRanker/src/main/java/SearchArtist.java
+++ SongRanker/src/main/java/SearchArtist.java
@@ -24,14 +24,18 @@ public class SearchArtist extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
 
-    response.setContentType("text/plain");
-    response.setCharacterEncoding("UTF-8");
+   
 
     String name = request.getParameter("artistName");
     
     if (name == "no") {
     	request.getRequestDispatcher("").forward(request, response);;
     }
+    
+    
+    
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
     response.getWriter().print(request.getParameter("artistName") + "\r\n");
 
   }
