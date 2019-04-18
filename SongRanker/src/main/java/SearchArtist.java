9a68bdd8e61c3e848be964a1447bb8c1f4bd7e61
jacob <jacob@128.211.188.46>
2019-04-18 17:05:26 -0400

    finally have firebase connected

--- SongRanker/src/main/java/SearchArtist.java
+++ SongRanker/src/main/java/SearchArtist.java
@@ -1,10 +1,25 @@
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 @WebServlet(
     name = "SearchArtist",
@@ -23,16 +38,38 @@ public class SearchArtist extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException, ServletException {
+	  
+	  InputStream serviceAccount = new FileInputStream("C:\\Users\\jacob\\git\\Song-Ranker\\SongRanker\\key.json");
+	  GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
+	  FirebaseOptions options = new FirebaseOptions.Builder()
+	      .setCredentials(credentials)
+	      .build();
+	  FirebaseApp.initializeApp(options);
 
-   
+	  Firestore db = FirestoreClient.getFirestore();
 
-    String name = request.getParameter("artistName");
+	  String aname = request.getParameter("artistName");
+	  
+	// Create a reference to the cities collection
+	  CollectionReference cities = db.collection("artists");
+	  // Create a query against the collection.
+	  Query query = cities.whereEqualTo("name", aname);
+	  // retrieve  query results asynchronously using query.get()
+	  ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+	  try {
+		for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
+		    System.out.println(document.getId());
+		  }
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (ExecutionException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
     
-    if (name == "no") {
-    	request.getRequestDispatcher("").forward(request, response);;
-    }
-    
-    
+    FirebaseApp.getInstance().delete();
     
     response.setContentType("text/plain");
     response.setCharacterEncoding("UTF-8");
